51f7f181ae4c5bb71febf01fd964f02d96ec0588
Dellan Muchengapadare <mactunechy@gmail.com>
2024-01-23 19:22:44 +0200

    request and respone qirg @api_view decorator

--- snippets_api/urls.py
+++ snippets_api/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('snippets', views.snippet_list),
-    path('snippets/<int:pk>', views.snippet_detail),
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+
+urlpatterns = format_suffix_patterns(urlpatterns)
